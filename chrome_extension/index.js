09da4e78aca92ef99c7b7e753e6a37774173bbb1
sghosalkar <shubhamghosalkar.sg@gmail.com>
2020-10-25 15:42:01 +0530

    Consume flask service on chrome extension

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,9 +1,46 @@
+var serverBaseUrl = "http://127.0.0.1:5000"
+
 var messages = [];
 
 function clickHandler(e) {
-  var x = "You: " + document.getElementById('message').value;
-  messages.push(x);
+  var x = document.getElementById('message').value;
+  messages.push("You: " + x);
+
+  var args = x.split(" ");
+  if (args[0].toUpperCase() == "PROJECT"){
+    var projectId = args[1]
+    if (args[2].toUpperCase() == "BUILD") {
+      var response = fetchBuilds(projectId)
+    } else if (args[2].toUpperCase() == "PIPELINE") {
+      var pipelineId = args[3]
+      var response = fetchPipeline(projectId, pipelineId)
+    }
+  }
+
   refreshView();
+}
+
+function fetchBuilds(projectId){
+  var url = serverBaseUrl + "/project/" + projectId + "/build"
+  return doHttpGetCall(url)
+}
+
+function fetchPipeline(projectId, pipelineId){
+  var url = serverBaseUrl + "/project/" + projectId + "/pipeline/" + pipelineId
+  return doHttpGetCall(url)
+}
+
+function doHttpGetCall(url){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var response = xhr.responseText;
+      messages.push("Bot: " + response);
+      refreshView();
+    }
+  }
+  xhr.send();
 }
 
 function createMessageItem(message){
--- flask_api/service.py
+++ flask_api/service.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from flask_api.azure_adapter import get_build_definitions, get_pipeline_definition
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
